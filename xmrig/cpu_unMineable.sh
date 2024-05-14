4c461cd5ebed49633b7a1a16eb9ad515ec30da6b
Bogdan SOLOMENCO <bSolomenco@gmail.com>
2024-05-14 21:27:37 +0300

    xmrig/cpu_unMineable.sh: simplify id

--- xmrig/cpu_unMineable.sh
+++ xmrig/cpu_unMineable.sh
@@ -4,4 +4,4 @@
 scriptDir=$(dirname "${BASH_SOURCE[0]}")
 echo "scriptDir: ${scriptDir}"
 
-${scriptDir}/bld/xmrig -a rx -o stratum+tcp://rx.unmineable.com:3333 -u LTC:ltc1qshmk2u694y60dg4guvn4saxkpkc9jzxq2dwu59.LTC_$(uname -n)_xmrig#0yr0-ywoq -p x --randomx-1gb-pages
+${scriptDir}/bld/xmrig -a rx -o stratum+tcp://rx.unmineable.com:3333 -u LTC:ltc1qshmk2u694y60dg4guvn4saxkpkc9jzxq2dwu59.$(uname -n)_xmrig#0yr0-ywoq -p x --randomx-1gb-pages
