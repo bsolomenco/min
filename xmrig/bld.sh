cf6fbeb6bcba7abda0182c566b46985380b2eb62
Bogdan SOLOMENCO <bSolomenco@gmail.com>
2025-05-31 19:23:50 +0300

    xmrig: cuda submodule,  build script

--- xmrig/bld.sh
+++ xmrig/bld.sh
@@ -2,6 +2,10 @@
 
 scriptDir=$(dirname "${BASH_SOURCE[0]}")
 
-rm -rf "${scriptDir}/bld"
-cmake -S "${scriptDir}/rep" -B "${scriptDir}/bld" -D"CMAKE_INSTALL_PREFIX=${scriptDir}"
-cmake --build "${scriptDir}/bld" --parallel $(nproc)
+rm -rf "${scriptDir}/.bld_xmrig"
+cmake -S "${scriptDir}/rep" -B "${scriptDir}/.bld_xmrig" -D"CMAKE_INSTALL_PREFIX=${scriptDir}"
+cmake --build "${scriptDir}/.bld_xmrig" --config=Release --parallel $(nproc)
+
+rm -rf "${scriptDir}/.bld_cuda"
+cmake -S "${scriptDir}/cuda" -B "${scriptDir}/.bld_cuda" -D"CMAKE_INSTALL_PREFIX=${scriptDir}"
+cmake --build "${scriptDir}/.bld_cuda" --config=Release --parallel $(nproc)
